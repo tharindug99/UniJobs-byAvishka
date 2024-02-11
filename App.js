b0867e0b74a043fdd7323116d95552267878215b
Avishka Kothalawala <avishkakothalawala99@gmail.com>
2024-02-11 13:14:30 +0530

    Update packages and App.js

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens';
+import { decode, encode } from 'base-64';
 
-import React from 'react'; 
-import Navigation from './Navigation/navigation.js';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'react-native-gesture-handler';
+import {firebase} from './src/firebase/config';
+import Navigation from './Navigation/navigation';
+import OptionsSelection from './src/screens/OptionsSelection/OptionSelection.js';
+
+
+
+if (!global.btoa) { global.btoa = encode; }
+if (!global.atob) { global.atob = decode; }
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  
   return (
-    <Navigation />
+    <>
+    <Navigation/>
+   
+    </>
   );
+  
+
 }
